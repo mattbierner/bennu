e8203bf261f475fa0e6afc8f5c6fbef3385b6367
Matt Bierner <mattbierner@gmail.com>
2012-11-28 15:55:19 -0500

    Renabled strict mode

--- test/testChoice.js
+++ test/testChoice.js
@@ -32,6 +32,19 @@ define(['parse'], function(parse){
                 
                 assert.throws(parse.run.bind(undefined, a, ""));
              }],
+             ["Choice Order",
+             function(){
+                var a = parse.choice(
+                    parse.string('a'),
+                    parse.string('aa'),
+                    parse.string('aaa'));
+                assert.deepEqual(parse.run(a, "aaaa"), 'a');
+                
+                assert.deepEqual(parse.run(parse.choice(
+                    parse.string('aaa'),
+                    parse.string('aa'),
+                    parse.string('a')), 'aaaa'), 'aaa');
+            }]
         ],
     };
 });
