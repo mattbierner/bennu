887ac595460f35fdd871889c62b7de15b3e6bc2c
Matt Bierner <mattbierner@gmail.com>
2013-01-17 13:32:08 -0500

    Added tests for runMany, fixed bad ref.

---
+++ test/testRunMany.js
@@ -0,0 +1,38 @@
+define(['parse/parse', 'parse/parse_eager', 'stream'], function(parse, parse_eager, stream){
+    var ab = parse.either(
+            parse.character('a'),
+            parse.character('b'));
+    
+    return {
+        'module': "Run Many Tests",
+        'tests': [
+            ["Consume many items",
+            function(){
+                assert.deepEqual(
+                    stream.toArray(parse.runMany(ab, "abbaab")),
+                    ['a', 'b', 'b', 'a', 'a', 'b']);
+                
+                assert.deepEqual(
+                    stream.toArray(parse.runMany(ab, "abbaab abba")),
+                    ['a', 'b', 'b', 'a', 'a', 'b']);
+            }],
+            ["Consume no items, return empty stream",
+            function(){                
+                assert.deepEqual(
+                    stream.toArray(parse.runMany(ab, "cd")),
+                    []);
+                
+                assert.deepEqual(
+                    stream.toArray(parse.runMany(ab, "")),
+                    []);
+             }],
+              ["Run on infinite stream",
+              function(){
+                  var s = stream.stream('a', function(){ return s; });
+                  
+                  var result = parse.runManyStream(parse.character('a'), s);
+                  assert.ok(true);
+              }]
+        ],
+    };
+});
