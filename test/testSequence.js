572c48f118217fa2d0197b8125fa8210ac7da23b
Matt Bierner <mattbierner@gmail.com>
2013-01-25 15:05:04 -0500

    Added m param to parser function for memoization use.

--- test/testSequence.js
+++ test/testSequence.js
@@ -8,6 +8,18 @@ define(['parse/parse', 'parse/parse_eager'], function(parse, parse_eager){
                 
                 assert.deepEqual(parse.run(a, "ab"), ['a', 'b']);
              }],
+             
+            ["fail Sequence",
+            function(){
+                assert.throws(parse.run.bind(undefined,
+                    parse_eager.sequence(parse.character('a'), parse.never(), parse.character('b')), "ab"));
+                
+                assert.throws(parse.run.bind(undefined,
+                    parse_eager.sequence(parse.character('a'), parse.character('b'), parse.never()), "ab"));
+                
+                assert.throws(parse.run.bind(undefined,
+                    parse_eager.sequence(parse.never(), parse.character('a'), parse.character('b')), "ab"));
+             }]
         ],
     };
 });
