64f558df175fab0c497a9a60aefef4f6e75f421d
Matt Bierner <mattbierner@gmail.com>
2013-01-27 12:15:21 -0500

    Added test for memo id share.

--- test/testMemo.js
+++ test/testMemo.js
@@ -19,6 +19,23 @@ define(['parse/parse'], function(parse){
                 
                 assert.deepEqual(g, 3);
             }],
+            ["Parser Memo",
+            function(){
+                var g = 0;
+                
+                var a = parse.Parser('a', parse.bind(parse.character('a'), function(x) {
+                    g++;
+                    return parse.always(x);
+                }));
+                
+                parse.run(
+                    parse.either(
+                        parse.attempt(parse.sequence(parse.memo(a), parse.memo(a), parse.never())),
+                        parse.sequence(parse.memo(a), parse.memo(a), parse.memo(a))),
+                    "aaa");
+                
+                assert.deepEqual(g, 3);
+            }],
             ["Nested Memo",
             function(){
                 var g = 0;
