33d61c6c07c48cb9907ff66f5ff0877b86d063cb
Matt Bierner <mattbierner@gmail.com>
2014-04-09 21:28:37 -0400

    Recompil

--- dist_node/parse.js
+++ dist_node/parse.js
@@ -1,7 +1,8 @@
 /*
- * THIS FILE IS AUTO GENERATED from 'lib/parse.kep'
+ * THIS FILE IS AUTO GENERATED FROM 'lib/parse.kep'
  * DO NOT EDIT
-*/"use strict";
+*/
+"use strict";
 var stream = require("nu-stream")["stream"],
     NIL = stream["NIL"],
     first = stream["first"],
